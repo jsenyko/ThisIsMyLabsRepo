0538da828f88c7d64d1ded81a1c9e2d04c35b498
Jakob <jakobsenyko@gmail.com>
2022-10-13 18:51:32 -0400

    push

---
+++ Lab3/lab.js
@@ -0,0 +1,17 @@
+let firstName = "Jakob";
+let lastName = "Senyko";
+
+let fullName = firstName.concat(lastName);
+
+console.log(fullName);
+
+let population = (5,644,300);
+
+console.log(population);
+
+let newpop = population *3;
+
+console.log(newpop);
+
+javaScriptIsCool === true;
+
