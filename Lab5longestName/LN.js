94a8c25b8a7dd08f7a61a9e2fae6f5cb0a0d61c0
Jakob <jakobsenyko@gmail.com>
2022-10-17 16:11:31 -0400

    push

---
+++ Lab5longestName/LN.js
@@ -0,0 +1,18 @@
+let name1 = "Beth"
+let name2 = "Ryan"
+let name3 = "Ronald"
+
+if((name3 < name2) && (name1 < name3)) {
+    console.log('Ronald has the longest name')
+}
+
+//was trying to write code here for names of the same length, but couldn't get it 
+if((name1 == name2) && (name2 == name3) (name1 == name3)) {
+    console.log('All the names are the same length')
+}
+
+
+//I know its some kind of if statement but I'm likely usign the wrong operaters
+if(name1 >= name2) {
+    console.log('Beth and Ryan have the same name length')
+}
