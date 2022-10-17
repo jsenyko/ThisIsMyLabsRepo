14504e05453457f9993483f956bc53a620b46f48
Jakob <jakobsenyko@gmail.com>
2022-10-16 20:42:01 -0400

    push

---
+++ Lab4heatingNcooling/HC.js
@@ -0,0 +1,20 @@
+let actualTemp = 65;
+
+let desiredTemp = 70;
+
+if(actualTemp < desiredTemp) {
+    console.log('Run heat')
+}
+else if(actualTemp > desiredTemp) {
+    console.log('Run A/C')
+}
+else{
+    console.log('Standby')
+}
+
+
+let tempCelsius = 12;
+
+targetUnit = "C", "F", "K";
+
+//gave the switch statement a try but couldn't get it.
