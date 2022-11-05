22a86584c58989aabc933e07bb6f1b073308d984
Jakob <jakobsenyko@gmail.com>
2022-11-04 21:04:11 -0400

    push

--- Lab7StudentSub/lab.js
+++ Lab7StudentSub/lab.js
@@ -22,4 +22,82 @@ function addSubmission(array, newName, newScore, newDate){
     
 }
 
-addSubmission(submissions, "Sally", 40, "2020-01-01");
+//addSubmission(submissions, "Sally", 40, "2020-01-01");
+
+
+
+////#3////
+
+function deleteSubmissionByIndex(array, index) {
+    array.splice(index, 1);
+    console.log(array);
+}
+//deleteSubmissionByIndex(submissions, 1);
+
+
+/////#4////
+
+// function deleteSubmissionByName(array, name) {
+//     let indexOfObject = array.findIndex(object) => {
+//         return object.name === name;
+//     }
+// }
+// array.splice(indexOfObject, 1);
+
+///#5////
+
+function editsubmission(array, index, score){
+    let newScore = array[index].score = score;
+    newScore >= 60 ? array[index].passed = true : array[index].passed = false;
+    return array;
+}
+
+///#6////
+
+function findSubmissionByName(array, name) {
+    let find = array.find((array) => {
+        return array.name === name;
+    });
+    console.log(find);
+}
+
+///#7///
+
+function findLowestScore(array){
+    array.forEach((x) => {
+        if(x.score < lowest.score){
+            lowest = x;
+        }
+    })
+    return lowest;
+}
+
+//#8//
+
+function findAverageScore(array){
+    let avgScore = 0; clearInterval;
+    for(x of array){
+        avgScore += x.score;
+    }
+    return x / submissions.length
+}
+
+//#9//
+function filterPassing(array){
+    return array.filter((value) => {
+        return value.passed;
+    });
+}
+//console.log(filterPassing(submissions))
+
+//#10//
+
+function filter90AndAbove(array){
+    let find = array.filter(function (submissions){
+        return submissions.score >= 90;
+    })
+
+    return find;
+}
+
+console.log(filter90AndAbove(submissions))
---
+++ Lab7StudentSub/test.js
@@ -0,0 +1,7 @@
+let employees = ["Bob", "James", "Joe", "Marcus", "Amelia", "Kyle", "Sandra"];
+
+// console.log(employees[10]);
+
+for(let x of employees){
+    console.log(x);
+}  
